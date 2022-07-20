ff69acdbc4a557a7faf143d114b6272423ca657a
rlagyalsdlek <rlagyalsdlek@naver.com>
2022-07-20 20:01:11 +0900

    fix kurento config

---
+++ backend/src/main/java/com/ssafy/api/service/CallService.java
@@ -0,0 +1,13 @@
+package com.ssafy.api.service;
+
+public class CallService {
+    // maven to gradle
+
+    // application.properties update
+
+    // ubuntu kms server (docker)
+
+    // One2OneCall code fix
+
+    // advanced, recording...
+}
