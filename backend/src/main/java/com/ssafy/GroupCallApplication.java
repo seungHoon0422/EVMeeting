2551ef9ed15728b32939cc7765f0324af76a3ee0
heejo <joy96817@naver.com>
2022-08-04 14:51:36 +0900

    Add env.properties

--- backend/src/main/java/com/ssafy/GroupCallApplication.java
+++ backend/src/main/java/com/ssafy/GroupCallApplication.java
@@ -3,6 +3,7 @@ package com.ssafy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -10,6 +11,7 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
+@PropertySource("classpath:properties/env.properties")
 public class GroupCallApplication {
 	public static void main(String[] args) {
         SpringApplication.run(GroupCallApplication.class, args);
