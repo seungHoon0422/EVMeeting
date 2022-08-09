8348d3456f3a4a6a43c4fa41b205f8b30893223d
BaekSeungHoon <psh104404@naver.com>
2022-08-09 13:19:05 +0900

    Update feature-chat, statistics controller

--- backend/src/main/java/com/ssafy/user/db/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/user/db/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.user.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -15,6 +16,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserid(String userid);
     Optional<User> findById(Long id);
     boolean existsByUserid(String userid);
-
-
+    @Override
+    List<User> findAll();
 }
